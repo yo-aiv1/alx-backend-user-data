6188cf6ca460abe86f6eb371cf2e2cfb9ca8e4a7
yahya <yahyaoulha0@gmail.com>
2024-05-29 12:41:36 +0100

    done

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -14,3 +14,12 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ checks if a password is hashed """
+    pwd = password.encode('utf-8')
+    if bcrypt.checkpw(pwd, hashed_password):
+        return True
+    else:
+        return False
