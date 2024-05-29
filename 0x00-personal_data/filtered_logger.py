409bb32b57ce2b3ab77ee7b0d09090d42447d9f1
yahya <yahyaoulha0@gmail.com>
2024-05-29 12:36:13 +0100

    d

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
+"""filter data"""
+
+
 import re
 
 
 def filter_datum(fields, redaction, message, separator) -> None:
-    """ filter_datum returns the log message obfuscated """
+    """
+    filter_datum returns the log message obfuscated
+    """
     return re.sub(separator.join(fields), redaction, message)
