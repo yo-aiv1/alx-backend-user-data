692bb8338cd4376d27aaf8fd0df0c1be6cfee323
yahya <yahyaoulha0@gmail.com>
2024-06-03 16:28:44 +0100

    done

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,24 +12,30 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+
 auth = None
-authtype = "auth"
-ExcludePaths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                '/api/v1/forbidden/']
-
-
-if authtype:
+auth_type = getenv('AUTH_TYPE')
+if auth_type == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
 
 
 @app.before_request
 def before_request() -> str:
-    if auth is None or not auth.require_auth(request.path, ExcludePaths):
+    """authentication middleware"""
+    if auth is None:
         return
-    elif auth.authorization_header(request) is None:
+
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/', '/api/v1/stat*']
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+
+    if auth.authorization_header(request) is None:
         abort(401)
-    elif auth.current_user(request) is None:
+
+    if auth.current_user(request) is None:
         abort(403)
 
 
