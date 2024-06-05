2a3f41b4c7e7b6ecd563818706ec9c1d7713dd42
yahia <yahyaoulha0@gmail.com>
2024-06-05 12:03:26 +0100

    worked

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -68,4 +68,4 @@ def ForbiddenAccess(error) -> str:
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port, debug=True)
+    app.run(host=host, port=port)
